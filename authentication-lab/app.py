baa6507e575fa7728fbb4b9edd6f3f5eeda5421c
saeedja24-meet <139681133+saeedja24-meet@users.noreply.github.com>
2023-07-23 12:15:07 +0300

    Update app.py
    not completed

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,12 +2,47 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
+config={  
+    apiKey: "AIzaSyDDt_BZP5_FE4v0WvFfxkvR9snMFKm9o_8",
+    authDomain: "firstone-e6dd6.firebaseapp.com",
+    projectId: "firstone-e6dd6",
+    storageBucket: "firstone-e6dd6.appspot.com",
+    messagingSenderId: "978481786131",
+    appId: "1:978481786131:web:a42460d9b38a6a3d787ee6",
+    measurementId: "G-JW3NXNX6B3"
+    }
+firebase=pyrebase.initialize_app(config)
+auth=firebase.auth()
+app=Flask(__name__)
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+   error = ""
+   if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+           return redirect(url_for('home'))
+       except:
+           error = "Authentication failed"
+   return render_template("signup.html")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('home'))
+        except:
+            error = "Authentication failed"
     return render_template("signin.html")
 
 
@@ -20,6 +55,11 @@ def signup():
 def add_tweet():
     return render_template("add_tweet.html")
 
+@app.route('/signout')
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
 
 if __name__ == '__main__':
     app.run(debug=True)
