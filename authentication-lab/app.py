e3e6be37f0adbbef0dbb71b1c1cfe4e72828b82b
saeedja24-meet <139681133+saeedja24-meet@users.noreply.github.com>
2023-07-24 16:25:22 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -21,16 +21,18 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-   error = ""
-   if request.method == 'POST':
+    error = ""
+    if request.method == 'POST':
        email = request.form['email']
        password = request.form['password']
        try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for("signup.html"))
+            return redirect(url_for("add_tweet"))
        except:
             error = "Authentication failed"
-   return render_template("signup.html")
+            return render_template("signup.html")
+    else:
+        return render_template("signup.html")
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -41,14 +43,16 @@ def signin():
         password = request.form['password']
         try:
             login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            return redirect(url_for('home'))
+            return redirect(url_for("add_tweet"))
         except:
             error = "Authentication failed"
-    return render_template("signin.html")
+            return render_template("signin.html")
+    else:
+        return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
-def signup():
+def signupGo():
     return render_template("signup.html")
 
 
